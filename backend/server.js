11eb3599a180dfdcfbfae0eb9dd03d9f880cc596
Harsh-Rajora <72harshrajora@gmail.com>
2026-02-06 10:13:29 +0530

    separate backend for render

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import { emp } from "./models/empSchema.js"
+
+let conn = mongoose.connect("mongodb://localhost:27017/Employee")
+conn.then(()=> console.log("Mongodb Connected Successfully")).catch(err => console.log(err))
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', async (req, res) => {
+    const data = new emp(req.body);
+    await data.save();
+  res.send('Data saved successfully!')
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
